85004c94ffec29011bbd7c4679b64a43358c3501
Jack Dowling <140137635+JackDowling-0@users.noreply.github.com>
2023-12-01 09:13:46 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,6 @@
 #include "lib.h"
 
 int main(){
-    Flash::FlashcardManager manager;
-    Flash::Parser parser;
-    
-    std::string l1 = "This is a :line of text";
-    std::string l2 = "        This is a :line of text";
-    std::string l3 = "    This is a :   line of text     ";
-
-    manager.addFlashcard(parser.parseLine(l1));
-    manager.addFlashcard(parser.parseLine(l2));
-    manager.addFlashcard(parser.parseLine(l3));
-
-    manager.displayCards();
 
     return 0;
 }
